2ca56ba702eda629bbed7482b736811b3d9cc939
saikrish32 <54880365+saikrish32@users.noreply.github.com>
2020-07-15 21:43:08 +0530

    Add files via upload

---
+++ assignment_2/q2.js
@@ -0,0 +1,15 @@
+// JS substring() method
+var str = "Hello world!";
+var res = str.substring(1, 4);
+console.log(res);
+
+//Array Splice()
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+console.log(fruits);
+
+//replace()
+var str1 = "Visit Microsoft!";
+var res1 = str1.replace("Microsoft", "W3Schools");
+console.log(res1);
+
