ac9d4764cbb24ef7139313ce534b7488f823b777
saikrish32 <54880365+saikrish32@users.noreply.github.com>
2020-07-21 14:01:06 +0530

    Add files via upload

---
+++ day5_assignment/qs.js
@@ -0,0 +1,85 @@
+//question 1
+console.log('----------------question1--------------------');
+
+// let arr = []
+// while (true){
+// let number = +prompt('enter the number');
+// if(number<=0){
+//     break
+// }
+// arr.push(number);
+
+// }
+// console.log(arr);
+
+
+// let odd_numbers = arr.filter(el=>el%2==1);
+// console.log(odd_numbers);
+
+// let cube_number = arr.map(function(el){
+//     return el**3;
+// });
+// console.log(cube_number);
+
+console.log('----------------question2--------------------');
+
+// class User 
+// {
+//     constructor(name,age,email){
+//         this.name = name;
+//         this.age = age;
+//         this.email = email;
+//         this.luCoins = 0;
+//         this.courses = [];
+//     }
+//     static greet(){
+//         console.log("hii there")
+//     }
+//     login(){
+//         console.log(` ${this.name} has loggged in`);
+//         return this;
+//     }
+//     logout(){
+//         console.log(` ${this.name} has loggged out`);
+//         return this;
+//     }
+
+//     addcoins(){
+//         this.luCoins++;
+//         console.log(`${this.name} has ${this.luCoins} coins`);
+//         return this;
+//     }
+    
+//     getDetails(){
+//         console.log(`Name is ${this.name},
+//         email is ${this.email}`);
+//     }
+// }
+
+
+
+// class Admin extends User{
+//     addCourse(user,course){
+//         user.courses.push(course);
+//         console.log(user);
+//     }
+//     removeCourse(user,course){
+//         user.courses.pop(course);
+//         console.log(user);
+//     }
+// }
+
+
+
+
+// let user1 = new User('Dilip',25,'dillip@gmail.com');
+
+// let admin = new Admin('Rio',25,'r@gmail.com')
+
+// admin.addCourse(user1,'JS');
+// admin.addCourse(user1,'Python');
+
+// admin.removeCourse(user1,'JS');
+
+
+console.log('----------------question3--------------------');
